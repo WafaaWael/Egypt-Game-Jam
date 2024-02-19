3040d8704738fbf503e2be2e50050a7879951735
Wafaawael <wafaawael2@gmail.com>
2024-02-19 18:07:15 +0200

    this all the work for the Audio Mixer

--- Egypt-Game-Jam-main/Egypt game Jam/Assets/Wafaa/Scripts/Audio/AudioManager.cs
+++ Egypt-Game-Jam-main/Egypt game Jam/Assets/Wafaa/Scripts/Audio/AudioManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static AudioManager instance;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private List<AudioClip> clipList;
+    [SerializeField] private AudioMixer mixer;
+    public const string Music_Key = "Music";
+    public const string SFX_Key = "SFX";
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        loadVolum();
+    }
+    public void eatSFX()
+    {
+        AudioClip clip = clipList[Random.Range(0, clipList.Count)];
+        audioSource.PlayOneShot(clip);
+    }
+    private void loadVolum()
+    {
+        float musicVolum = PlayerPrefs.GetFloat(Music_Key, 1);
+        float SFXVolum = PlayerPrefs.GetFloat(SFX_Key, 1);
+
+        mixer.SetFloat(VolumeSetting.MIX_MUSIC, Mathf.Log10(musicVolum) * 20);
+        mixer.SetFloat(VolumeSetting.MIX_SFX, Mathf.Log10(SFXVolum) * 20);
+
     }
 }
--- Egypt-Game-Jam-main/Egypt game Jam/Assets/Wafaa/Scripts/Audio/VolumeSetting.cs
+++ Egypt-Game-Jam-main/Egypt game Jam/Assets/Wafaa/Scripts/Audio/VolumeSetting.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
 public class VolumeSetting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider Music;
+    [SerializeField] private Slider SFM;
+    public const string MIX_MUSIC = "Music";
+    public const string MIX_SFX = "SFX";
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Music.onValueChanged.AddListener(setMusic);
+        SFM.onValueChanged.AddListener(setSFX);
+
+    }
+    private void Start()
+    {
+        Music.value = PlayerPrefs.GetFloat(AudioManager.Music_Key, 1);
+        SFM.value = PlayerPrefs.GetFloat(AudioManager.SFX_Key, 1);
+
+    }
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(AudioManager.Music_Key, Music.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_Key, SFM.value);
+
+    }
+    private void setMusic(float value)
+    {
+        audioMixer.SetFloat(MIX_MUSIC, Mathf.Log10(value) * 20);
+    }
+    private void setSFX(float value)
+    {
+        audioMixer.SetFloat(MIX_SFX, Mathf.Log10(value) * 20);
     }
 }
